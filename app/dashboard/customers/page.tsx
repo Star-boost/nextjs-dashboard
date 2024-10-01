868da0967c1500d7f68c199a533a8d50213a03e7
Akindu Dasanayake <akindu@Akindus-MacBook-Pro.local>
2024-10-01 10:20:30 -0700

    finished course

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,5 +1,28 @@
+import CustomersTable from '@/app/ui/customers/table';
+import { Suspense } from 'react';
+import { Metadata } from 'next';
+import { fetchCustomers, fetchFilteredCustomers } from '@/app/lib/data';
+import Pagination from '@/app/ui/invoices/pagination';
+import Search from '@/app/ui/search';
+import { lusitana } from '@/app/ui/fonts';
+import { FormattedCustomersTable } from '@/app/lib/definitions';
 
-
-export default function Page() {
-    return <p>Customers Page</p>
+export const metadata: Metadata = {
+  title: 'Customers',
 }
+
+export default async function Page( {
+    searchParams,
+  }: {
+    searchParams?: {
+      query?: string;
+    };
+  }) {
+    const query = searchParams?.query || '';
+    const customers: FormattedCustomersTable[] = await fetchFilteredCustomers(query);
+    return (
+        <Suspense fallback={<p>Loading...</p>}> 
+            <CustomersTable customers={customers}> </CustomersTable>
+        </Suspense>
+    );
+  }
